f1b58c0b3610aefe55242684ed6275963ffc6f03
Saiprasad <saiprasad26102004@gmail.com>
2026-07-21 18:01:54 +0530

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "2et-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/icon1.png",
+  "/icon2.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
